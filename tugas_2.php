51c56249e458c1b0e8f8149f48eacfe1e72527b1
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 10:11:33 +0700

    Add files via upload

---
+++ tugas_2.php
@@ -0,0 +1,47 @@
+<?php
+
+$cireng="Rp.1.000";
+$es_duren="Rp.15.000";
+$es_milo="Rp.20.000";
+$es_teh="Rp.5.000";
+$piscok="Rp.1.000";
+
+
+echo "<h1>Daftar Menu</h1>";
+echo "</br>";
+echo "<table border=1>
+<tr>
+<td> No </td>
+<td> Menu Makanan </td>
+<td> Harga </td>
+</tr>
+<tr>
+<td> 1 </td>
+<td> Cireng </td>
+<td> .$cireng </td>
+</tr>
+<tr>
+<td> 2 </td>
+<td> es_duren </td>
+<td> .$es_duren </td>
+</tr>
+<tr>
+<td> 3 </td>
+<td> es_milo </td>
+<td> .$es_milo </td>
+</tr>
+<tr>
+<td> 4 </td>
+<td> es_teh </td>
+<td> .$es_teh </td>
+</tr>
+<tr>
+<td> 5 </td>
+<td> piscok </td>
+<td> .$piscok </td>
+</tr>
+
+
+
+</table>"
+?>
